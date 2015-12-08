c05a6db73c0dc9c5c3d3161420a3b7cb6c949f1a
Jessica <jessica.clayton.a@gmail.com>
2015-12-07 19:37:05 -0800

    three classes added

---
+++ classes.py
@@ -0,0 +1,17 @@
+class Food(object):
+
+	def ___init___(self, name, color):
+		self.name = name
+		self.color = color
+
+class Animals(object):
+
+	def ___int___ (self, kind, habitat):
+		self.kind = kind
+		self.habit = habitat 
+
+class Plants(object):
+
+	def ___init___(self, kind, color):
+		self.kind = kind
+		self.color = color
